8d469e816d45cad236912ff35cf6ab03f3d2b218
apokalipsa44 <m_lachowicz@wp.pl>
2020-03-20 22:44:34 +0100

    user saved to scheema

--- services/passportConfig.js
+++ services/passportConfig.js
@@ -1,12 +1,17 @@
 const passport = require('passport');
 const keys = require('../config/keys');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
-    }, accessToken => {
-        console.log(accessToken)
+    }, (accessToken, refreshToken, profile) => {
+        new User({
+            googleId: profile.id
+        }).save();
     }));
+
