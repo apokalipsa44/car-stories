d07d78c84c3ec3070bd372ace4513d23b420b3e2
apokalipsa44 <m_lachowicz@wp.pl>
2020-03-22 00:33:13 +0100

    users logic

--- services/passportConfig.js
+++ services/passportConfig.js
@@ -9,9 +9,17 @@ passport.use(
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
-    }, (accessToken, refreshToken, profile) => {
-        new User({
-            googleId: profile.id
-        }).save();
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({googleId: profile.id}).then((existingUser) => {
+            if (existingUser) {
+                //user - get that user
+                done(null, existingUser);
+
+            } else {
+                //saves new user and preforms user - user get user
+                new User({googleId: profile.id}).save()
+                    .then(user => done(null, user));
+            }
+        })
     }));
 
