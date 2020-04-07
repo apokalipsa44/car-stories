9c7102e876a0657e1c32188cc62907d6d1d7ffab
apokalipsa44 <m_lachowicz@wp.pl>
2020-04-07 23:58:31 +0200

    xy mddla addd

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,16 @@ class Header extends Component {
     render() {
         return (
             <div>
-                hadrereded
+
+                <nav>
+                    <div className="nav-wrapper">
+                        <a href="#" className="brand-logo">Kubota</a>
+                        <ul id="nav-mobile" className="right hide-on-med-and-down">
+                            <li><a href="sass.html">Login</a></li>
+
+                        </ul>
+                    </div>
+                </nav>
             </div>
         );
     }
