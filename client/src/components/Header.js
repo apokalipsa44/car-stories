504aa3ed831834f22e7af3955623a88348bcaf73
apokalipsa44 <m_lachowicz@wp.pl>
2020-08-03 00:04:00 +0200

    code cleanup, serviceworker removed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 class Header extends Component {
-    render() {
-        return (
-            <div>
-
-                <nav>
-                    <div className="nav-wrapper">
-                        <a href="/auth/google" className="brand-logo">google Login</a>
-                        <ul id="nav-mobile" className="right hide-on-med-and-down">
-                            <li><a href="sass.html">kkk</a></li>
-
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <nav>
+          <div className="nav-wrapper">
+            <a href="/auth/google" className="brand-logo">
+              google Login
+            </a>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li>
+                <a href="sass.html">kkk</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Header;
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,16 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) {app.use(
-    createProxyMiddleware("/auth/google", { target: "http://localhost:5000/" }));
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/auth/google", { target: "http://localhost:5000/" })
+  );
 };
-
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-// module.exports = function(app) {
-//     app.use(
-//         '/api/*',
-//         createProxyMiddleware({
-//             target: 'http://localhost:5000',
-//             changeOrigin: true,
-//         })
-//     );
-// };
