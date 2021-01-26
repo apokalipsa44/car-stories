abb3e49469903d86fdf284dae1756b39c1e97d68
apokalipsa44 <m_lachowicz@wp.pl>
2021-01-26 14:13:06 +0100

    mongo connection added

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
+
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  () => console.log("db connected")
+);
 
 app.get("/", (req, res) => {
   res.send("jojojo");
