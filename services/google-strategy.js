3baa90617a486aa0e1cac64063cb6f986d54d379
apokalipsa44 <m_lachowicz@wp.pl>
2021-02-02 13:15:06 +0100

    auth google keys hide

--- services/google-strategy.js
+++ services/google-strategy.js
@@ -1,13 +1,17 @@
 var passport = require('passport');
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+require("dotenv").config();
+
 
 // Use the GoogleStrategy within Passport.
 //   Strategies in passport require a `verify` function, which accept
 //   credentials (in this case, a token, tokenSecret, and Google profile), and
 //   invoke a callback with a user object.
 passport.use(new GoogleStrategy({
-    clientID: "725221755584-mhc90pn1mjhuc5iake2sr1rmjmo094v6.apps.googleusercontent.com",
-    clientSecret: "vlORC6_FxDd9hlEx2JsKNb0A",
+    // clientID: "725221755584-mhc90pn1mjhuc5iake2sr1rmjmo094v6.apps.googleusercontent.com",
+    clientID: process.env.GOOGLE_CONSUMER_KEY,
+    // clientSecret: "vlORC6_FxDd9hlEx2JsKNb0A",
+    clientSecret: process.env.GOOGLE_CONSUMER_SECRET,
     callbackURL: "http://localhost:5000/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
