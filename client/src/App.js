f4d82b75ce71087d767bd8bb5f1151e9b09b0d04
apokalipsa44 <m_lachowicz@wp.pl>
2021-02-15 23:16:29 +0100

    tailwind done

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
-import React from "react";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import MainPage from "./components/MainPage";
+import React from 'react';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <MainPage />
-      <Footer />
+    <div >
+     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">
+  Button
+</button>
     </div>
   );
 }
