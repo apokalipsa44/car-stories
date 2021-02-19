f81ea7af996a9151ce36bab53117256ecadda27f
apokalipsa44 <m_lachowicz@wp.pl>
2021-02-19 12:56:03 +0100

    tailwind remove

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,6 @@
+const plugins = require('tailwind-react-ui/plugins')
+
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
@@ -7,5 +10,8 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss/lib/plugins/container')({}),
+    ...Object.keys(plugins).map(name => plugins[name]()),
+  ],
 }
