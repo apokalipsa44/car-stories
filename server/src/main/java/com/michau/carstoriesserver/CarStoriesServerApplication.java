c3a5ca1658b1d9d88c718b106cf42f84752cb144
apokalipsa44 <m_lachowicz@wp.pl>
2022-01-17 23:31:08 +0100

    project cleanup

---
+++ server/src/main/java/com/michau/carstoriesserver/CarStoriesServerApplication.java
@@ -0,0 +1,13 @@
+package com.michau.carstoriesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarStoriesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarStoriesServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/michau/carstoriesserver/CarStoriesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michau.carstoriesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarStoriesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
