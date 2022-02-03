d159eb392bbe0143b07ae31c0d9e1d74a3d41a4b
apokalipsa44 <m_lachowicz@wp.pl>
2022-02-03 20:21:47 +0100

    models done

---
+++ car-stories-client/src/app/shared/enums/bikeTags.enum.ts
@@ -0,0 +1,13 @@
+export enum BikeTags {
+    SPORT = "Sports bike",
+    CHOPPER ="Chopper",
+    SUPERMOTO = "Supermoto",
+    STUNT = "Stunt bike",
+    CLASSIC = "Classic bike",
+    OLDTIMER = "Oldtimer motorcycle",
+    CROSS = "Cross bike", 
+    ENDURO = "Enduro",
+    CUSTOM = "Custom bike",
+    STREET_FIGHTER = "StreetFighter",
+    NAKED = "Naked bike"
+}
---
+++ car-stories-client/src/app/shared/enums/vehicleTypes.enum.ts
@@ -0,0 +1,4 @@
+export enum VehicleTypes {
+    CAR ="Car",
+    MOTORCYLCE ="Motorcycle"
+}
