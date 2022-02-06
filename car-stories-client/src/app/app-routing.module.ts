cf2e50720c5bb18621ad46f14057ff87ca65f456
apokalipsa44 <m_lachowicz@wp.pl>
2022-02-07 00:51:53 +0100

    fixed routng

--- car-stories-client/src/app/app-routing.module.ts
+++ car-stories-client/src/app/app-routing.module.ts
@@ -4,21 +4,21 @@ import { Route, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: '/story', pathMatch: 'full' },
   {
-    path: '/story',
+    path: 'story',
     loadChildren: () =>
       import('./../app/UI/story-view/story-view.module').then(
         (m) => m.StoryViewModule
       ),
   },
   {
-    path: '/user',
+    path: 'user',
     loadChildren: () =>
       import('./../app/UI/user-options-view/user-options-view.module').then(
         (m) => m.UserOptionsViewModule
       ),
   },
   {
-    path: '/store',
+    path: 'store',
     loadChildren: () =>
       import('./../app/UI/shop-view/shop-view.module').then(
         (m) => m.ShopViewModule
