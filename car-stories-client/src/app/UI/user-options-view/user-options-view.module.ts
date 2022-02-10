16a06aa1ea01a538953efc04d2aa7f2f64d09d75
apokalipsa44 <m_lachowicz@wp.pl>
2022-02-10 21:47:15 +0100

    updated to scss

--- car-stories-client/src/app/UI/user-options-view/user-options-view.module.ts
+++ car-stories-client/src/app/UI/user-options-view/user-options-view.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserOptionsViewRoutingModule } from './user-options-view-routing.module';
+import { UserFormComponent } from './user-container/user-form/user-form.component';
+import { UserContainerComponent } from './user-container/user-container.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserFormComponent,
+    UserContainerComponent
+  ],
   imports: [
     CommonModule,
     UserOptionsViewRoutingModule
